4ebb8840fa8b5044cf1410f049b6614922719105
Jan Bormet <jan.bormet@bormet.info>
2023-06-16 18:52:16 +0200

    Setup Hello World fiber project

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"log"
+	"os"
+)
+
+func SetLogFile(path string) {
+	logFile, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	log.SetOutput(logFile)
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	err := app.Listen(":3000")
+	if err != nil {
+		log.Fatal(err)
+	}
+}
