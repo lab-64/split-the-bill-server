8ffdd1758bc8a676e8f8475a5bbd09371cdaaa8d
felix <f.o.d.c@web.de>
2023-11-23 15:03:04 +0100

    #56 Include date field for bill entity

--- storage/database/entity/bill.go
+++ storage/database/entity/bill.go
@@ -3,18 +3,20 @@ package entity
 import (
 	"github.com/google/uuid"
 	. "split-the-bill-server/domain/model"
+	"time"
 )
 
 type Bill struct {
 	Base
-	Name    string    `gorm:"not null"`
+	Name    string `gorm:"not null"`
+	Date    time.Time
 	Items   []Item    `gorm:"foreignKey:BillID"` // has many items
 	GroupID uuid.UUID `gorm:"type:uuid"`         // belongs to group
 }
 
 // ToBillEntity converts a BillModel to a Bill
 func ToBillEntity(bill BillModel) Bill {
-	return Bill{Base: Base{ID: bill.ID}, Name: bill.Name, GroupID: bill.Group}
+	return Bill{Base: Base{ID: bill.ID}, Name: bill.Name, Date: bill.Date, GroupID: bill.Group}
 }
 
 // ToBillModel converts a Bill to a BillModel
@@ -26,5 +28,5 @@ func ToBillModel(bill Bill) BillModel {
 		items = append(items, ToItemModel(item))
 	}
 
-	return BillModel{ID: bill.ID, Name: bill.Name, Items: items}
+	return BillModel{ID: bill.ID, Name: bill.Name, Date: bill.Date, Group: bill.GroupID, Items: items}
 }
