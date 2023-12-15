d4ba2e23f075b2e98376bb0676fda6d854e1fa4d
felix <f.o.d.c@web.de>
2023-12-15 18:01:14 +0100

    #93 Include group owner to get all users

--- storage/database/db_storages/user_storage.go
+++ storage/database/db_storages/user_storage.go
@@ -28,7 +28,10 @@ func (u *UserStorage) GetAll() ([]UserModel, error) {
 	var users []User
 	// find all users in the database
 	// TODO: GetAllUsers should not return an error, if no users are found
-	tx := u.DB.Preload("Groups.Members").Preload("GroupInvitations").Find(&users)
+	tx := u.DB.
+		Preload("Groups.Owner").
+		Preload("Groups.Members").
+		Preload("GroupInvitations").Find(&users)
 	if tx.Error != nil {
 		return nil, tx.Error
 	}
