b468dff66c194e3b50668a27a522061360030826
felix <f.o.d.c@web.de>
2024-02-14 19:11:40 +0100

    #106 Delete all group associations on group delete

--- storage/database/db_storages/group_storage.go
+++ storage/database/db_storages/group_storage.go
@@ -102,8 +102,22 @@ func (g *GroupStorage) GetGroups(userID uuid.UUID, invitationID uuid.UUID) ([]mo
 }
 
 func (g *GroupStorage) DeleteGroup(id uuid.UUID) error {
-	res := g.DB.Delete(&entity.Group{}, "id = ?", id)
-	if res.Error != nil {
+
+	group := entity.Group{
+		Base: entity.Base{ID: id},
+	}
+	err := g.DB.Transaction(func(tx *gorm.DB) error {
+		// Delete the group
+		res := g.DB.Delete(&group)
+		if res.Error != nil {
+			return res.Error
+		}
+		// Delete all of a group's has one, has many, and many2many associations
+		res = g.DB.Select(clause.Associations).Delete(&group)
+		return res.Error
+	})
+
+	if err != nil {
 		return storage.NoSuchGroupError
 	}
 	return nil
---
+++ storage/database/db_storages/group_storage_test.go
@@ -0,0 +1,56 @@
+package db_storages
+
+import (
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"split-the-bill-server/domain/model"
+	"testing"
+)
+
+var TestGroup = model.Group{
+	ID:   uuid.New(),
+	Name: "Test Group",
+}
+
+func TestGroupStorage_DeleteGroup(t *testing.T) {
+
+	tests := []struct {
+		name        string
+		groupID     uuid.UUID
+		mock        func()
+		expectedErr error
+	}{
+		{
+			name:    "Success",
+			groupID: TestGroup.ID,
+			mock: func() {
+				dbMock.ExpectBegin()
+				dbMock.ExpectBegin()
+				dbMock.ExpectExec(`UPDATE "groups" SET "deleted_at"`).WithArgs(sqlmock.AnyArg(), TestGroup.ID).WillReturnResult(sqlmock.NewResult(1, 1))
+				dbMock.ExpectCommit()
+				dbMock.ExpectBegin()
+				dbMock.ExpectExec(`DELETE FROM "group_members"`).WithArgs(TestGroup.ID).WillReturnResult(sqlmock.NewResult(1, 1))
+				dbMock.ExpectExec(`UPDATE "bills" SET "deleted_at"`).WithArgs(sqlmock.AnyArg(), TestGroup.ID).WillReturnResult(sqlmock.NewResult(1, 1))
+				dbMock.ExpectExec(`UPDATE "group_invitations" SET "deleted_at"`).WithArgs(sqlmock.AnyArg(), TestGroup.ID).WillReturnResult(sqlmock.NewResult(1, 1))
+				dbMock.ExpectExec(`UPDATE "groups" SET "deleted_at"`).WithArgs(sqlmock.AnyArg(), TestGroup.ID).WillReturnResult(sqlmock.NewResult(1, 1))
+				dbMock.ExpectCommit()
+				dbMock.ExpectCommit()
+			},
+			expectedErr: nil,
+		},
+	}
+
+	for _, testcase := range tests {
+		t.Run(testcase.name, func(t *testing.T) {
+			testcase.mock()
+			err := groupStorage.DeleteGroup(testcase.groupID)
+			assert.Equalf(t, testcase.expectedErr, err, "Wrong error")
+
+			// Ensure all expectations were met
+			if err = dbMock.ExpectationsWereMet(); err != nil {
+				t.Errorf("Unfulfilled expectations: %s", err)
+			}
+		})
+	}
+}
