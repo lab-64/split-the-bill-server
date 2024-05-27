0fd3d33a6755ce59ebd240a1e014fcc697e93806
felix <f.o.d.c@web.de>
2024-05-27 12:59:55 +0200

    #188 Delete unused billID on item

--- presentation/dto/item.go
+++ presentation/dto/item.go
@@ -13,7 +13,6 @@ type ItemInput struct {
 	ID           uuid.UUID           `json:"id"`
 	Name         *string             `json:"name,omitempty"`
 	Price        *float64            `json:"price,omitempty"`
-	BillID       uuid.UUID           `json:"billId"`
 	Contributors *Changes[uuid.UUID] `json:"contributorIDs,omitempty"`
 }
 
@@ -36,12 +35,8 @@ func (i ItemInput) ValidateInputs() error {
 	if *i.Price == 0 {
 		return ErrItemPriceRequired
 	}
-	if i.BillID == uuid.Nil {
-		return ErrItemBillIDRequired
-	}
 	return nil
 }
 
 var ErrItemNameRequired = errors.New("name is required")
 var ErrItemPriceRequired = errors.New("price is required")
-var ErrItemBillIDRequired = errors.New("billID is required")
