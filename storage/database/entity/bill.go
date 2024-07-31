180b9eb288339cc4a48c509548676795f5897bb7
felix <f.o.d.c@web.de>
2024-07-31 19:35:13 +0200

    #204 Enable Automatic User Association Deletion

--- storage/database/entity/bill.go
+++ storage/database/entity/bill.go
@@ -8,10 +8,10 @@ import (
 type Bill struct {
 	Base
 	OwnerID    uuid.UUID `gorm:"type:uuid"`
-	Owner      User      `gorm:"foreignKey:OwnerID"` // belongs to user
+	Owner      User      `gorm:"foreignKey:OwnerID; constraint:OnUpdate:CASCADE, OnDelete:CASCADE"` // belongs to user
 	Name       string    `gorm:"not null"`
 	Date       time.Time
-	Items      []Item    `gorm:"foreignKey:BillID; constraint:OnUpdate:CASCADE,OnDelete:CASCADE"` // has many items
-	GroupID    uuid.UUID `gorm:"type:uuid"`                                                       // group has many bills
-	UnseenFrom []User    `gorm:"many2many:unseen_bills"`                                          // many to many unseen bills
+	Items      []Item    `gorm:"foreignKey:BillID; constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`       // has many items
+	GroupID    uuid.UUID `gorm:"type:uuid"`                                                             // group has many bills
+	UnseenFrom []User    `gorm:"many2many:unseen_bills; constraint:OnUpdate:CASCADE, OnDelete:CASCADE"` // many to many unseen bills
 }
--- storage/database/entity/group.go
+++ storage/database/entity/group.go
@@ -8,8 +8,8 @@ type Group struct {
 	Base
 	Name            string          `gorm:"not null"`
 	OwnerUID        uuid.UUID       `gorm:"type:uuid"`
-	Owner           User            `gorm:"foreignKey:OwnerUID"`
-	Members         []*User         `gorm:"many2many:group_members; constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
+	Owner           User            `gorm:"foreignKey:OwnerUID; constraint:OnUpdate:CASCADE, OnDelete:CASCADE"` // belongs to user
+	Members         []*User         `gorm:"many2many:group_members; constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 	Bills           []Bill          `gorm:"foreignKey:GroupID; constraint:OnUpdate:CASCADE,OnDelete:CASCADE"` // has many bills
 	GroupInvitation GroupInvitation `gorm:"foreignKey:GroupID; constraint:OnUpdate:CASCADE,OnDelete:CASCADE"` // has one invitation
 }
